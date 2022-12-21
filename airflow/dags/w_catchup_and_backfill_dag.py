fe31ed08b383ec77b2d0830f9555872e3c28f9c9
Vadzim <vadzimk@hotmail.com>
2022-12-21 05:56:13 -0800

    dag using catchup argument

---
+++ airflow/dags/w_catchup_and_backfill_dag.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(dag_id="dag_with_catchup_and_backfill_v03",
+         start_date=datetime.now()-timedelta(hours=3), schedule_interval="@hourly",
+         catchup=False  # by default is True and will execute missed times from the start_date till now
+         ) as dag:
+    task1 = BashOperator(task_id="task1", bash_command="echo this is a simple bash command")
+
+
+# docker exec -it <scheduler> bash
+# airflow dags backfill -s <start_date> -e <end_date> <dag_id>  # allows to execute missed times from the past
