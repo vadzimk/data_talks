4267135f50a132e934741afbdee3ef1eb197eae0
Vadzim <vadzimk@hotmail.com>
2022-12-21 04:51:02 -0800

    dag passing multiple return values between tasks

--- airflow/dags/w_python_operator.py
+++ airflow/dags/w_python_operator.py
@@ -11,11 +11,19 @@ def greet_one(name):
 def greet_two(ti):
     """ :param ti: The task instance"""
     name = ti.xcom_pull(task_ids='get_name')  # task_ids parameter specifies from which task to pull the return value
+    number = ti.xcom_pull(task_ids='get_address', key="number")
+    street = ti.xcom_pull(task_ids='get_address', key="street")
     print(f"Hello from {name} python operator")
+    print(f"My address is {number} {street}")
 
 
 def get_name():
-    return "Frog"
+    return "Frog"  # by default every task return value will be pushed to XCom, max size of it is only 48 KBytes, so it is not suitable for dataframes
+
+
+def get_address(ti):
+    ti.xcom_push(key="number", value="1")
+    ti.xcom_push(key="street", value="North Pole")
 
 
 default_args = {
@@ -24,12 +32,11 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-with DAG(default_args=default_args, dag_id="dag_with_python_operator_v4", description="first dag with python operator",
+with DAG(default_args=default_args, dag_id="dag_with_python_operator_v5", description="first dag with python operator",
          start_date=datetime.now(), schedule_interval="@once"):
     task0 = PythonOperator(task_id="greet_task", python_callable=greet_one, op_kwargs={'name': 'Gypsy'})
     task1 = PythonOperator(task_id="get_name", python_callable=get_name)
-    task2 = PythonOperator(task_id="greet_task_using_return_value", python_callable=greet_two)
+    task2 = PythonOperator(task_id="get_address", python_callable=get_address)
+    task3 = PythonOperator(task_id="greet_task_using_return_value", python_callable=greet_two)
 
-    task1 >> task2
-
-#  by default every tasks retrun value will be pushed to XComs
+    [task1, task2] >> task3
