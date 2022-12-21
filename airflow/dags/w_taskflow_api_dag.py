0b8ef440cf5823e5adf7d59010e27837950333ad
Vadzim <vadzimk@hotmail.com>
2022-12-21 05:28:22 -0800

    dag using taskflow api and multiple_outputs

--- airflow/dags/w_taskflow_api_dag.py
+++ airflow/dags/w_taskflow_api_dag.py
@@ -8,21 +8,23 @@ default_args = {
 }
 
 
-@dag(dag_id="dag_with_taskflow_api_v3", default_args=default_args, start_date=datetime.now(), schedule_interval="@once")
+@dag(dag_id="dag_with_taskflow_api_v4", default_args=default_args, start_date=datetime.now(), schedule_interval="@once")
 def hello_etl():
     @task
     def get_name():
         return "Gypsy"
 
-    @task
+    @task(multiple_outputs=True)  # need multiple_outputs=True to pass the dict attributes separately to the
+    # consuming task
     def get_address():
         return {"number": 1, "street": "North Pole"}
 
     @task
-    def greet(name, address):
-        print(f"Hello, I am {name}, address: {address['number']} {address['street']}")
+    def greet(name, number, street):
+        print(f"Hello, I am {name}, address: {number} {street}")
 
-    greet(name=get_name(), address=get_address())
+    address_dict = get_address()
+    greet(name=get_name(), number=address_dict['number'], street=address_dict['street'])
 
 
 greet_dag = hello_etl()
