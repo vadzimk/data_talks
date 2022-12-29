ba27b1d818a464c7c3fce81b07f8d58529b024b7
Vadzim <vadzimk@hotmail.com>
2022-12-29 12:07:52 -0800

    add metabase service

---
+++ airflow/scripts/init.sql
@@ -0,0 +1,5 @@
+create database metabaseappdb;
+CREATE DATABASE test;
+CREATE DATABASE airflow;
+-- GRANT ALL PRIVILEGES ON DATABASE airflow TO admin;
+-- GRANT ALL PRIVILEGES ON DATABASE test TO admin;
